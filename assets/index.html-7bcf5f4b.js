d177682bf4193190f8f654ecb94e4bcf5eef551d
liguojing <liguojing@tal.com>
2023-06-21 15:14:31 +0800

    add blogs

---
+++ assets/index.html-7bcf5f4b.js
@@ -0,0 +1,8 @@
+import{_ as a}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c as l,f as e,e as o,a as i}from"./app-0a4091d1.js";const n="/assets/fe/network-osi.png",r={},s=o('<h1 id="网络" tabindex="-1"><a class="header-anchor" href="#网络" aria-hidden="true">#</a> 网络</h1><h2 id="osi七层模型" tabindex="-1"><a class="header-anchor" href="#osi七层模型" aria-hidden="true">#</a> OSI七层模型</h2><ul><li>应用层</li><li>表示层</li><li>会话层</li><li>传输层</li><li>网络层</li><li>数据链路层</li><li>物理层</li></ul><h2 id="tcp-ip四层模型" tabindex="-1"><a class="header-anchor" href="#tcp-ip四层模型" aria-hidden="true">#</a> TCP/IP四层模型</h2><ul><li>应用层</li><li>传输层</li><li>网络层</li><li>链路层</li></ul>',5),c=i("figure",null,[i("img",{src:n,alt:"网络分层说明",tabindex:"0",loading:"lazy"}),i("figcaption",null,"网络分层说明")],-1);function d(h,u){return t(),l("div",null,[e(`
+ * @Author: liguojing liguojing@tal.com
+ * @Date: 2023-06-19 21:45:01
+ * @LastEditors: liguojing liguojing@tal.com
+ * @LastEditTime: 2023-06-21 11:31:30
+ * @FilePath: /github/codingdocs/src/fe/network/index.md
+ * @Description: 这是默认设置,请设置\`customMade\`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+`),s,e(" 图片说明 "),c])}const f=a(r,[["render",d],["__file","index.html.vue"]]);export{f as default};
